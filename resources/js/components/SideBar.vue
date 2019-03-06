72f9240f3713c88e1184396337c181e509a43d5f
Vipin Ramani Raghavan <vipins110@gmail.com>
2019-03-06 14:26:58 +0530

    App vue is implemented

---
+++ resources/js/components/SideBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item">Menu 1</li>
+        <li class="list-group-item">Menu 2</li>
+        <li class="list-group-item">Menu 3</li>
+    </ul>    
+</template>
+<script>
+export default {
+    name : 'SideBar'
+}
+</script>
