a6ae52f9f212ff8cb568626ea67c3ddbaf595ba1
Vipin Ramani Raghavan <vipins110@gmail.com>
2019-03-06 17:53:44 +0530

    Security in routing - integrated

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::fallback(function(){
+
+    if( Auth::check())
+        return view('home');
+    else
+        return redirect()->route('login');
+});
